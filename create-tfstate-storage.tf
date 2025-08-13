112702233084b6c92225e8454e61c2e69447ef9d
Harsh Patel <81179105+coffeeemu873761@users.noreply.github.com>
2025-08-13 07:24:56 -0500

    fixed storage container name

--- create-tfstate-storage.tf
+++ create-tfstate-storage.tf
@@ -36,7 +36,7 @@ resource "azurerm_storage_account" "tfstate" {
 # Create a storage container
 resource "azurerm_storage_container" "tfstate" {
     name = "tfstate"
-    storage_account__id = azurerm_storage_account.tfstate.id
+    storage_account_name = azurerm_storage_account.tfstate.name
     container_access_type = "private"
 }
 
